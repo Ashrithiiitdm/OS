ea79fbded4c9c049da627a8ba1b31a40516d459d
Ashrithiiitdm <cs22b2025@iiitdm.ac.in>
2024-11-09 23:44:06 +0530

    Completed memoryinfo system call

---
+++ Project1_xv6CustomizeSystemCalls/xv6-public/getprocesstype.d
@@ -0,0 +1,2 @@
+getprocesstype.o: getprocesstype.c /usr/include/stdc-predef.h types.h \
+ stat.h user.h fcntl.h
---
+++ Project1_xv6CustomizeSystemCalls/xv6-public/memusage.d
@@ -0,0 +1,2 @@
+memusage.o: memusage.c /usr/include/stdc-predef.h types.h stat.h user.h \
+ fcntl.h
